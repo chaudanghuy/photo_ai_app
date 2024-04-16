d04a5f7805d5cc2b90641bb7d09bf414ae335a44
chaudanghuy686868 <160588142+chaudanghuy686868@users.noreply.github.com>
2024-04-16 16:13:00 +0700

    Localization

--- app/src/screens/Print.js
+++ app/src/screens/Print.js
@@ -4,10 +4,28 @@ import { useNavigate } from 'react-router-dom';
 import i18n from '../translations/i18n';
 import "../css/Print.css";
 
+// Background
+import background_en from '../assets/Prints/BG.png';
+import background_kr from '../assets/Prints/kr/BG.png';
+import background_vn from '../assets/Prints/vn/BG.png';
+
 function Print() {
      const { t } = useTranslation();
      const navigate = useNavigate();
      const [hoveredImage, setHoveredImage] = useState(null);
+
+     const [background, setBackground] = useState(background_en);
+
+     useEffect(() => {
+          const storedLanguage = sessionStorage.getItem('language');
+          if (storedLanguage === 'en') {
+               setBackground(background_en);
+          } else if (storedLanguage === 'ko') {
+               setBackground(background_kr);
+          } else if (storedLanguage === 'vi') {
+               setBackground(background_vn);
+          }
+     }, [])
 
      const handleMouseEnter = (image) => {
           setHoveredImage(image);
@@ -29,7 +47,7 @@ function Print() {
      }, []);
 
      return (
-          <div className='print-container'></div>
+          <div className='print-container' style={{ backgroundImage: `url(${background})` }}></div>
      );
 }
 
