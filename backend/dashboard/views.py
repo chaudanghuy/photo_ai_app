88420441259c17365bf0dc3782888ba79dab6af0
Châu Đăng Huy <chaudanghuy@gmail.com>
2024-04-18 12:53:59 +0700

    Backend

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -3,27 +3,41 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from revenue.models import Order, Transaction
 from device.models import Device
+from store.models import Store
 
 # Create your views here.
 
 class Dashboard(LoginRequiredMixin, View):    
     def get(self, request):
-        # Transaction
-        transactions = Transaction.objects.order_by('-id')
-        total_amount = sum(t.amount for t in transactions)
         
-        # Order
-        total_orders = transactions.count()
+        # List stores
+        stores = Store.objects.all()
         
-        # Device
-        devices = Device.objects.all()
-        total_devices_online = sum(1 for device in devices if device.status == 'Online')
-        
-        # Get list of transactions sorted by id        
+        # List devices by store
+        devices = Device.objects.all()               
         
         return render(request, 'dashboard.html', {
-            'total_amount': total_amount,
-            'total_orders': total_orders,
-            'total_devices_online': total_devices_online,
-            'transactions': transactions
-        })
+            'stores': stores,
+            'devices': devices,            
+        })            
+        
+
+class DashboardStores(LoginRequiredMixin, View):
+    def get(self, request, deviceID):
+        # Get store device by store id
+        device = Device.objects.get(id=deviceID)
+        
+        if (device):
+            # Get list orders by device id
+            orders = Order.objects.filter(device_id=device.id).all()
+            
+            # Get list transaction by order id
+            transactions = Transaction.objects.filter(order_id__in=orders).order_by('-id')
+            total_amount = sum(t.amount for t in transactions)
+            
+            return render(request, 'dashboard-stores.html', {
+                'device': device,
+                'total_amount': total_amount,
+                'transactions': transactions
+            })
+        return render(request, 'dashboard-stores.html')
