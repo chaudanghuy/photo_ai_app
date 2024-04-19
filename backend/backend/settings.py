a1be992258010e3609a5924bfe380b4eab36f3d8
chaudanghuy686868 <160588142+chaudanghuy686868@users.noreply.github.com>
2024-04-19 10:01:30 +0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,7 +53,7 @@ SECRET_KEY = 'django-insecure-mk2xtk=yk6e7g%ro2xoh-h+7e0=-@s(*ri+j@6qf61cu$=l4e%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['139.180.209.110', '127.0.0.1']
+ALLOWED_HOSTS = ['139.180.209.110', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -172,7 +172,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+if not DEBUG:
+    STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
